859dbf916dec0526d06401df840f661c50b81ad6
Bernardo Borges <bernardobbm334@gmail.com>
2023-03-22 11:14:48 -0300

    content loading settings and pages

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,37 @@
+import { useEffect, useState } from 'react';
+import { mapData } from '../../api/map-data';
 import { Base } from '../Base';
 import { mockBase } from '../Base/mock';
+import { Loading } from '../Loading';
+import { PageNotFound } from '../PageNotFound';
+
+type DataType = ReturnType<typeof mapData>[0];
 
 export function Home() {
+  const [data, setData] = useState<DataType | undefined>({} as DataType);
+
+  useEffect(() => {
+    async function loadDataFromApi() {
+      try {
+        const data = await fetch(
+          'http://localhost:1337/api/pages?populate=deep',
+        );
+        const dataJson = await data.json();
+
+        const pageData = mapData(dataJson)[0];
+
+        setData(pageData);
+      } catch (e) {
+        setData(undefined);
+      }
+    }
+
+    loadDataFromApi();
+  }, []);
+
+  if (data && !data?.slug) return <Loading />;
+
+  if (data === undefined) return <PageNotFound />;
+
   return <Base {...mockBase} />;
 }
